be25a7a4d40fd7d7d5c41e8b6b934a56767e66f8
nojuskk <nojus.krauklys@gmail.com>
2022-09-29 20:49:25 +0300

    Upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI highScoreText;
+    public TextMeshProUGUI scoreText;
+    public GameObject endGamePanel;
+
+    float timer;
+    float highScoreTimer;
+
+	// Use this for initialization
+	void Start () {
+        highScoreTimer = float.PositiveInfinity;
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            highScoreTimer = PlayerPrefs.GetFloat("HighScore");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        timerText.text = timer.ToString("F2");
+        if(GameObject.FindGameObjectsWithTag("Collectable").Length == 0)
+        {
+            if(timer < highScoreTimer)
+            {
+                highScoreTimer = timer;
+                PlayerPrefs.SetFloat("HighScore", timer);
+            }
+            highScoreText.text = highScoreTimer.ToString("F2");
+            scoreText.text = timer.ToString("F2");
+            endGamePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        rb.AddForce(movement * speed);
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("Collectable"))
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
